8dc6f1942c2f4a4de0ba2f2d25aac3191cabfa22
Chris Bentzel <cbentzel@alum.mit.edu>
2010-06-09 17:25:35 -0400

    Different fonts for different systems.

--- my-lisp/myfont.el
+++ my-lisp/myfont.el
@@ -1,11 +1,12 @@
 (require 'color-theme)
-(require 'emacs-type)
 
 (setq color-theme-is-global t)
 (color-theme-initialize)
 (color-theme-goldenrod)
 
-(if-not-win32 (set-default-font "-bitstream-bitstream vera sans mono-medium-r-normal--10-0-0-0-m-0-iso8859-9"))
+(if (eq system-type 'windows-nt)
+    (set-default-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
+  (set-default-font "-bitstream-bitstream vera sans mono-medium-r-normal--10-0-0-0-m-0-iso8859-9"))
 
 (require 'highlight-80+)
 
