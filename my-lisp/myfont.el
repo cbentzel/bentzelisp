3924a1e3333ad154b90447365711d659249558c8
Chris Bentzel <cbentzel@alum.mit.edu>
2010-06-23 13:19:04 -0400

    New font display.

--- my-lisp/myfont.el
+++ my-lisp/myfont.el
@@ -2,11 +2,11 @@
 
 (setq color-theme-is-global t)
 (color-theme-initialize)
-(color-theme-goldenrod)
+(color-theme-katester)
 
 (if (eq system-type 'windows-nt)
-    (set-default-font "-outline-Consolas-normal-r-normal-normal-14-97-96-96-c-*-iso8859-1")
-  (set-default-font "-bitstream-bitstream vera sans mono-medium-r-normal--10-0-0-0-m-0-iso8859-9"))
+  (set-default-font "-outline-Consolas-normal-r-normal-normal-12-97-96-96-c-*-iso8859-1")
+  (set-default-font "Inconsolata-8"))
 
 (require 'highlight-80+)
 
